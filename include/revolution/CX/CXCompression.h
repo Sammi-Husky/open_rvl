a8735e4d8b4ecc047d85b0fd9d24bd69452e6f57
mparisi20 <mparisi@stevens.edu>
2025-08-30 23:51:51 -0400

    Add more functions/types to CXCompression.h

--- include/revolution/CX/CXCompression.h
+++ include/revolution/CX/CXCompression.h
@@ -7,8 +7,30 @@
 extern "C" {
 #endif
 
+typedef struct CXStream {
+    u8 m_type : 4; // compression type
+    u8 pad : 4;
+    u32 m_size : 24; // 24-bit uncompressed size, in little-endian
+    u32 m_size32; // 32-bit uncompressed size, in little-endian
+    // TODO more members?
+} CXStream;
+// TODO size
+
+typedef struct CXUncompContext {
+    u8 unk0[0x30];
+} CXUncompContext;
+// TODO size
+
+void CXInitUncompContextRL(CXUncompContext* ctx, void* p2);
+void CXInitUncompContextLZ(CXUncompContext* ctx, void* p2);
+void CXInitUncompContextHuffman(CXUncompContext* ctx, void* p2);
+
+s32 CXReadUncompRL(CXUncompContext* ctx, const CXStream* src, void* dest);
+s32 CXReadUncompLZ(CXUncompContext* ctx, const CXStream* src, void* dest);
+s32 CXReadUncompHuffman(CXUncompContext* ctx, const CXStream* src, void* dest);
+
 void CXUncompressLZ(void* data, void* destination);
-u32 CXGetUncompressedSize(void* data);
+u32 CXGetUncompressedSize(const void* data);
 
 #ifdef __cplusplus
 }
